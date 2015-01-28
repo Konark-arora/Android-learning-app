2fa10eb893666331ef2fbf166a2a336e90cd4300
Konark Arora <konark.arora@meritnation.com>
2015-01-28 09:58:30 +0530

    code refactoring

--- src/com/kids/fun2learn/utils/NetworkUtils.java
+++ src/com/kids/fun2learn/utils/NetworkUtils.java
@@ -1,38 +1,27 @@
-
 package com.kids.fun2learn.utils;
 
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 
-public class NetworkUtils
-	{
-
-		public static boolean isConnected(Context context)
-			{
-				
-				ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
-				// Checking for WiFi
-				NetworkInfo wifiNetwork = cm.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
-				if (wifiNetwork != null && wifiNetwork.isConnected())
-					{
-						return true;
-					}
-
-				// Checking for Mobile Data Internet
-				NetworkInfo mobileNetwork = cm.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
-				if (mobileNetwork != null && mobileNetwork.isConnected())
-					{
-						return true;
-					}
-
-				// Checking for any other type of Network Connectivity
-				NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
-				if (activeNetwork != null && activeNetwork.isConnected())
-					{
-						return true;
-					}
-				return false;
-			}
-
+public class NetworkUtils {
+	public static boolean isConnected(Context context) {
+		ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+		// Checking for WiFi
+		NetworkInfo wifiNetwork = cm.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+		if (wifiNetwork != null && wifiNetwork.isConnected()) {
+			return true;
+		}
+		// Checking for Mobile Data Internet
+		NetworkInfo mobileNetwork = cm.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
+		if (mobileNetwork != null && mobileNetwork.isConnected()) {
+			return true;
+		}
+		// Checking for any other type of Network Connectivity
+		NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+		if (activeNetwork != null && activeNetwork.isConnected()) {
+			return true;
+		}
+		return false;
 	}
+}
--- src/com/kids/fun2learn/utils/RandomNumberGenerator.java
+++ src/com/kids/fun2learn/utils/RandomNumberGenerator.java
@@ -1,39 +1,17 @@
 package com.kids.fun2learn.utils;
 
-
 public class RandomNumberGenerator {
-
-	public static int genarateRandomNumber(int range) {//3
-
-		 int randomNumber = 0;
-		 for (int i = 0; i < range; i++)
-		 randomNumber = (int) (Math.random() * range);
-		 return randomNumber;
-
-		
-		
-	}
-	
-	
-	
-	
-	public static int genarateRandomNumber1(int range) {//3
-
-		 int randomNumber = 1;
-		 for (int i = 1; i <=range; i++)
-		 randomNumber = (int) (Math.random() * range);
-		 return randomNumber;
-
-		
-		
+	public static int genarateRandomNumber(int range) {// 3
+		int randomNumber = 0;
+		for (int i = 0; i < range; i++)
+			randomNumber = (int) (Math.random() * range);
+		return randomNumber;
 	}
 
-	// {
-	// int randomNumber=0;
-	// for(int i=0;i<3;i++)
-	// randomNumber = (int)(Math.random()*3);
-	// return randomNumber;//generates 0,1,2,
-
-	// }
-
+	public static int genarateRandomNumber1(int range) {// 3
+		int randomNumber = 1;
+		for (int i = 1; i <= range; i++)
+			randomNumber = (int) (Math.random() * range);
+		return randomNumber;
+	}
 }
